16c8a283bd43b8d65dcee3557b866f75c51fe8b9
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2022-01-01 15:36:56 +0100

    Flexible Routing code
    code refactoring for flexibility with useRouteMatch and useLocation in components

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighLightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -10,6 +10,7 @@ const DUMMY_QUOTES = [
 
 const QuoteDetails = () => {
   const params = useParams();
+  const match = useRouteMatch();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -20,15 +21,18 @@ const QuoteDetails = () => {
   return (
     <Fragment>
       <HighLightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      {/* <Route path={`/quotes/${params.quoteId}`} exact> */}
+      <Route path={`${match.path}`} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          {/* <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}> */}
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
 
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      {/* <Route path={`/quotes/${params.quoteId}/comments`}> */}
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
